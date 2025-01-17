461da47b10749c5983ce22b68e59815c5fa0e051
Burhanuddin-Kachwala <burhanuddin.ast@gmail.com>
2025-01-17 13:37:46 +0530

    tailwind-grid
    converted the table tag into grid using tailwind classes

--- portfolio with tailwind/tailwind.config.js
+++ portfolio with tailwind/tailwind.config.js
@@ -1,18 +1,19 @@
 module.exports = {
   content: [
     "./src/**/*.{html,js,jsx,ts,tsx}",
-    "index.html"
+    "./index.html"
   ],
   theme: {
     screens: {
-      'sm': '380px',
-      'md': '480px',
-      'lg': '768px',
+      sm: '480px',
+      md: '640px',
+      lg: '768px',
+      xl: '1024px',
     },  
     extend: {
-      colors:{
-        primary: '#064e7e'
-      }
+      colors: {
+        primary: '#064e7e',
+      },
     },
   },
   plugins: [],
